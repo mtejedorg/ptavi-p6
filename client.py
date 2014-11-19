97726028b8be4d063ec2130b6c2504be737aad06
Marco Tejedor González <m.tejedorg@alumnos.urjc.es>
2014-11-19 13:23:34 +0100

    Implementada inicialización de client.py y algunas funciones básicas

--- client.py
+++ client.py
@@ -5,24 +5,60 @@ Programa cliente que abre un socket a un servidor
 """
 
 import socket
+import sys
+
+USAGE = "Usage: python client.py method receiver@IP:SIPport"
+SERV_ERROR = "Error: No server listening"
 
 # Cliente UDP simple.
 
-# Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
+coms = sys.argv
 
-# Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+# Cerramos el programa si no nos llaman con el número deseado de argumentos, informando del uso
+
+if len(coms) != 3:
+    print USAGE
+    sys.exit()
+
+# Inicializamos el programa e interpretamos los comandos
+METODO = coms[1].upper()  #Nos pueden pasar el parámetro en minúsculas
+servidor = coms[2]
+datos = servidor.split("@")
+NOMBRE = datos[0]
+direccion = datos[1].split(":")
+SERVER = direccion[0]
+PORT = direccion[1]
+if METODO == "BYE" or "INVITE":
+    LINE = METODO + " sip:" + NOMBRE + ":" + SERVER + " SIP/2.0\r\n"
+else:
+    print "Error, método no contemplado"
+    print "Métodos contemplados: INVITE, BYE"
+    sys.exit()
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((SERVER, PORT))
 
-print "Enviando: " + LINE
-my_socket.send(LINE + '\r\n')
-data = my_socket.recv(1024)
+try:
+    my_socket.connect((SERVER, int(PORT)))
+except socket.gaierror: #Cuando la IP es inválida
+    print "Error: invalid IP"
+    print USAGE
+    sys.exit() 
+except ValueError:  #Cuando el puerto no es un número
+    print "Error: invalid port"
+    print USAGE
+    sys.exit()
+
+print "Enviando: " + line
+my_socket.send(line + '\r\n')
+
+try:
+    data = my_socket.recv(1024)
+except socket.error:    #Cuando el servidor no existe
+    print "Error: No server listening at",
+    print SERVER + " port " + PORT
+    sys.exit()
 
 print 'Recibido -- ', data
 print "Terminando socket..."
