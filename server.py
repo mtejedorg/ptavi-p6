6aaa419deda77a0cf86b8c11428d57e41484db41
Marco Tejedor González <m.tejedorg@alumnos.urjc.es>
2014-11-24 11:00:38 +0100

    Implementado error por número incorreco de parámetros

--- server.py
+++ server.py
@@ -5,19 +5,20 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import SocketServer
+import sys
 
 
-class EchoHandler(SocketServer.DatagramRequestHandler):
+class SIPHandler(SocketServer.DatagramRequestHandler):
     """
-    Echo server class
+    SIP server class
     """
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
         self.wfile.write("Hemos recibido tu peticion")
+        line = self.rfile.read()
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
-            line = self.rfile.read()
             print "El cliente nos manda " + line
 
             # Si no hay más líneas salimos del bucle infinito
@@ -25,7 +26,16 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
                 break
 
 if __name__ == "__main__":
+
+    USAGE = "Usage: python server.py IP port audio_file"
+
     # Creamos servidor de eco y escuchamos
-    serv = SocketServer.UDPServer(("", 6001), EchoHandler)
-    print "Lanzando servidor UDP de eco..."
-    serv.serve_forever()
+    if len(sys.argv) == 4:
+        IP = sys.argv[1]
+        PORT = int(sys.argv[2])
+        FILE = sys.argv[3]
+        s = SocketServer.UDPServer((IP, PORT), SIPHandler)
+        print "Listening...\r\n"
+        s.serve_forever()
+    else:
+        print USAGE
