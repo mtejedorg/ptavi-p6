32c082862a242ef32b2dea8e4b5c6a3e61f82a05
Marco Tejedor González <m.tejedorg@alumnos.urjc.es>
2014-11-24 17:27:50 +0100

    Implementados procedimientos básicos y parte del handle

--- server.py
+++ server.py
@@ -12,18 +12,52 @@ class SIPHandler(SocketServer.DatagramRequestHandler):
     """
     SIP server class
     """
+    def mensaje(self, code):
+        """ 
+        Devuelve un string con la forma del mensaje a enviar
+        Halla el método a partir del código
+        """
+        if code == "100":
+            metodo = "Trying"
+        elif code == "180":
+            metodo = "Ringing"
+        elif code == "200":
+            metodo = "OK"
+        elif code == "400":
+            metodo = "Bad Request"
+        elif code == "405":
+            metodo = "Method not allowed"
+
+        msg = "SIP/2.0 " + code + metodo + "\r\n"
+        return msg
+
+    def send(self, code):
+        """ Envía al servidor un mensaje usando el código como parámetro """
+        msg = mensaje(code)
+        print "Enviando: " + msg
+        self.wfile.write(msg + '\r\n')
+        
 
     def handle(self):
-        # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write("Hemos recibido tu peticion")
+        """ Recibe los mensajes y se encarga de responder """
         line = self.rfile.read()
-        while 1:
-            # Leyendo línea a línea lo que nos envía el cliente
-            print "El cliente nos manda " + line
-
-            # Si no hay más líneas salimos del bucle infinito
-            if not line:
-                break
+        print "El cliente nos manda ==> " + line
+        metodo = line.split()[0]
+        prot = line.split()[2]
+        if metodo == "INVITE":
+            self.send("100")    # Envío Trying
+            self.send("180")    # Envío Ringing
+            self.send("200")    # Envío OK
+            answer = self.rfile.read() # Recibo ACK
+            answer = line.split()[0]
+            if answer != "ACK":
+                print "Conversación rechazada por el cliente"
+        elif metodo == "BYE":
+            self.send("200")
+        elif prot != "SIP/2.0":
+            self.send("400")
+        else:
+            self.send("405")
 
 if __name__ == "__main__":
 
